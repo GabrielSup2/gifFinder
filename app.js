9be3d1d8d1582125484a2af5c5bffa85527e5229
GabrielSup2 <gabriel_ppedro@hotmail.com>
2022-07-29 13:45:55 -0300

    Refactoring functions

--- app.js
+++ app.js
@@ -1,33 +1,75 @@
 const form = document.querySelector("form");
+const divWIthGifs = document.querySelector("div")
+const apiKey = "johLqC4sWIRKESKJrvTUVrapjS23Gw9U"
 
-form.addEventListener("submit", async event=>{
-  event.preventDefault()
-  const inputValue = event.target.search.value
-  const apiKey = "johLqC4sWIRKESKJrvTUVrapjS23Gw9U"
-  const url = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&limit=1&q=${inputValue}`
-  const divWIthGifs = document.querySelector("div")
+const getApiUrl = GIFName => {
+  return `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&limit=1&q=${GIFName}`
+}
 
-try{
-  const response = await fetch(url)
+const generateGifImg = (gifImageUrl, gifData) => {
 
-  if(!response.ok){
-    throw new Error("Não foi possível obter os dados!")
+  const img = document.createElement("img")
+  img.setAttribute("src", gifImageUrl)
+  img.setAttribute("alt", gifData.data[0].title)
+
+  return img
+
+}
+
+const gifFetch = async (apiUrl) => {
+
+  try {
+
+    const response = await fetch(apiUrl)
+
+    if (!response.ok) {
+      throw new Error("Não foi possível obter os dados!")
+
+    }
+    return await response.json()
+
+
+  } catch (error) {
+    alert(`Erro na requisição : ${error.message}`)
+  }
+
+
+}
+
+const insertGifIntoDom = async (apiUrl) => {
+
+
+
+
+  const gifData = await (await fetch(apiUrl)).json()
+
+  if(gifData) {
+    
+    const gifImageUrl = gifData.data[0].images.downsized.url
+  
+    const img = generateGifImg(gifImageUrl, gifData)
+  
+    divWIthGifs.insertAdjacentElement("afterbegin", img)
+  
+    form.reset()
 
   }
 
-const gifData = await response.json()
-const gifImageUrl = gifData.data[0].images.downsized.url
-const img = document.createElement("img")
-img.setAttribute("src", gifImageUrl)
-img.setAttribute("alt", gifData.data[0].title)
-console.log (img)
-divWIthGifs.insertAdjacentElement("afterbegin", img)
-}catch(error){
-  alert(`Erro na requisição : ${error.message}`)
 }
 
 
 
+form.addEventListener("submit", async event => {
+  event.preventDefault()
 
-  
+  const inputValue = event.target.search.value
+
+  const apiUrl = getApiUrl(inputValue)
+
+  gifFetch(apiUrl)
+
+
+  insertGifIntoDom(apiUrl)
+
+
 })
