6c8eee9d1aa44e6d0defc16b025e917668d12abf
GabrielSup2 <gabriel_ppedro@hotmail.com>
2022-07-29 12:45:37 -0300

    Js functions implemented

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ form.addEventListener("submit", async event=>{
   const inputValue = event.target.search.value
   const apiKey = "johLqC4sWIRKESKJrvTUVrapjS23Gw9U"
   const url = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&limit=1&q=${inputValue}`
+  const divWIthGifs = document.querySelector("div")
 
 try{
   const response = await fetch(url)
@@ -14,10 +15,14 @@ try{
 
   }
 
-const GIFdata = await response.json()
-console.log(GIFdata)
-  
-} catch(error){
+const gifData = await response.json()
+const gifImageUrl = gifData.data[0].images.downsized.url
+const img = document.createElement("img")
+img.setAttribute("src", gifImageUrl)
+img.setAttribute("alt", gifData.data[0].title)
+console.log (img)
+divWIthGifs.insertAdjacentElement("afterbegin", img)
+}catch(error){
   alert(`Erro na requisição : ${error.message}`)
 }
 
